3158912485187d60190a1cec4963380bfe104d45
Bootstrapkat <bootstrapkat@users.noreply.github.com>
2018-12-15 11:45:29 -0500

    docs

---
+++ docs/precache-manifest.13ace9b652cd67cab9cb6e56285344bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ce01a715329d6017c47b",
+    "url": "/static/js/main.ce01a715.chunk.js"
+  },
+  {
+    "revision": "b5e3c18ecb41eaecff99",
+    "url": "/static/js/1.b5e3c18e.chunk.js"
+  },
+  {
+    "revision": "ce01a715329d6017c47b",
+    "url": "/static/css/main.7dbc25b5.chunk.css"
+  },
+  {
+    "revision": "d7b353b665b4f6e3c1f9d6f191add4d3",
+    "url": "/index.html"
+  }
+];
